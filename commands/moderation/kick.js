eee0a17546d0a06f6aba31f98c49a1bf58c63d27
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-11 17:55:58 -0500

    Add files via upload

---
+++ commands/moderation/kick.js
@@ -0,0 +1,46 @@
+  
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "kick",
+  description: "Kick a specified user from the server",
+  category: "moderation",
+  usage: "<user id> <reason>",
+  run: async (bot, message, args) => {
+    if (!args[0])
+      return message.channel.send(
+        `Please specify who you wish to kick! (Please give the user id or username)`
+      );
+    let User = message.guild.members.cache.get(args[0]);
+    if (!User)
+      return message.channel.send(
+        `That is not a user in the server.`
+      );
+    let Reason = message.content.split(`!kick ${User.id} `);
+    if (!args[1])
+      return message.channel.send(
+        `Please specify a reason.`
+      );
+    if (!Reason)
+      return message.channel.send(
+        `Please specify a reason.`
+      );
+    if (!User.kickable)
+      return message.channel.send(
+        `You can not kick this user, they may have a role higher then me or the same role as me.`
+      );
+    if (!message.member.permissions.has("KICK_MEMBERS"))
+      return message.channel.send(
+        `You do not have the KICK_MEMBERS permission!`
+      );
+    User.kick(Reason);
+    const Embed = new MessageEmbed()
+      .setTitle(`You have kicked a member!`)
+      .setDescription(
+        `You have kicked the user ${
+          bot.users.cache.get(User.id).username
+        } from this server!`
+      )
+      .setColor(1752220);
+    message.channel.send(Embed);
+  },
+};
