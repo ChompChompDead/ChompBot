dc44e2f14ce65c93f9e8f7113f649695cd218bb2
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-12 16:56:10 -0500

    Add files via upload

---
+++ commands/info/helpmath.js
@@ -0,0 +1,13 @@
+const {MessageEmbed} = require('discord.js')
+
+module.exports={
+    name: "help math",
+    category: "info",
+    description: "Help with math commands",
+    run: async(bot,message,args)=>{
+        const Embed = new MessageEmbed()
+        .setTitle('🔢 All Math Commands')
+        .setDescription(`\n\n**+** is to multiply\n**-** is to subtract\n** * ** is to multiply\n**/** is to subtract\n^ is to power a number by another number.`)
+        .setFooter('Math Commands | Still a ChompBot Message.')
+    }
+}
