d99745ca461a2e3bf926200819c31961240af839
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-12 17:02:11 -0500

    Update math.js

--- commands/mathfolder/math.js
+++ commands/mathfolder/math.js
@@ -8,6 +8,6 @@ module.exports={
         if(!args[0]) return message.channel.send('Please give me your first number.')
         if(!args[1]) return message.channel.send('You did not give me an operation to perform.')
         if(!args[2]) return message.channel.send('Please give me your second number.')
-        message.channel.send(calculator(args[0], args[1], args[2]))
+        message.channel.send(calculator(args[0],args[1],args[2]))
     }
 }
