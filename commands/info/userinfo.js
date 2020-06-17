ef5c8217272b29676f9fb965a002157795096454
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-17 15:14:33 -0500

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,40 +1,20 @@
-const { MessageEmbed } = require("discord.js");
-const { formatDate } = require("../../functions");
-module.exports = {
-  name: "userinfo",
-  description: "Get your own or someone else's user info.",
-  usage: "[user mention]",
-  category: "fun",
-  run: async (bot, message, args) => {
-    let Embed = new MessageEmbed();
-    let roles = [];
-    if (!message.mentions.users.first()) {
-      message.member.roles.cache.forEach((role) => {
-        roles.push(role.name);
-      });
-      Embed.setTitle(`Your info!`);
-      Embed.setThumbnail(message.author.displayAvatarURL());
-      Embed.setColor(1752220);
-      Embed.setDescription(
-        `**Joined Server At:** (MM/DD/YYYY) ${formatDate(message.member.joinedAt)}\n**User ID:** ${
-          message.author.id
-        }\n**Roles:** ${roles}`
-      );
-      return message.channel.send(Embed);
-    } else {
-      let User = message.mentions.members.first();
-      User.roles.cache.forEach((role) => {
-        roles.push(role.name);
-      });
-      Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s info!`);
-      Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
-      Embed.setColor(1752220);
-      Embed.setDescription(
-        `**Joined Server At:** (MM/DD/YYYY) ${formatDate(User.joinedAt)}\n**User ID:** ${
-          User.id
-        }\n**Roles:** ${roles}`
-      );
-      return message.channel.send(Embed);
+const Discord = require('discord.js')
+
+module.exports={
+    name: "userinfo",
+    description: "Someones info.",
+    run: async(bot,message,args)=>{
+        const user = message.mentions.users.first() || message.author;
+        let uEmbed = new Discord.MessageEmbed()
+        .setColor(`BLUE`)
+        .setTitle("User Info")
+        .setThumbnail(bot.users.cache.get(user.id).displayAvatarURL())
+        .setAuthor(`${message.author.username}'s Info!`, bot.users.cache.get(user.id).displayAvatarURL())
+        .addField("**Username:**", `${message.author.username}`,true)
+        .addField("**User Tag:**", `${message.author.discriminator}`, true)
+        .addField("**ID:**", `${message.author.id}`,true)
+        .addField("**Status:**", `${message.author.presence.status}`, true)
+        .addField("**Account Created At:**", `${message.author.createdAt}`,true)
+        message.channel.send(uEmbed)
     }
-  },
-};
+}
