f5cb9021b8793e1bf4fb746cc0727f4ea08c8af9
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-18 12:11:44 -0500

    Add files via upload

---
+++ commands/moderation/ban.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+
+module.exports={
+    name: "ban",
+    description: "Bans someone.",
+    usage: "<@user> <reason>",
+    run: async(bot,message,args)=>{
+        if(!message.member.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send(`You do not have the correct permissions to do this command, ${message.author}`)
+        }
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send(`You do not have the correct permissions to do this command, ${message.author}`)
+        }
+
+        let target = message.mentions.members.first();
+
+        if(!target) {
+            return message.channel.send(`Please specify who you want to ban, ${message.author}`)
+        }
+        if(!target.banable) {
+            return message.channel.send(`I can not ban this user because they have a role higher than me.`)
+        }
+        if(target.id === message.author.id) {
+            return message.channel.send(`You can not ban yourself, dummy.`)
+        }
+        if(!args[1]) {
+            return message.channel.send(`Please specify a reason to ban someone. Don't be stupid.`)
+        }
+
+        const Embed = new Discord.MessageEmbed()
+        .setColor(`BLUE`)
+        .setTitle(`**__${message.author.username}__ has banned someone :wave:**`)
+        .setDescription(`${message.author} has banned ${target} (${target.id})`)
+        .setFooter(`Well, goodbye for them.`)
+        message.channel.send(Embed)
+
+        target.ban(args[1])
+    }
+}
---
+++ commands/moderation/kick.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js')
+
+module.exports={
+    name: "kick",
+    description: "Kicks someone.",
+    usage: "<@user> <reason>",
+    run: async(bot,message,args)=>{
+        if(!message.member.hasPermission("KICK_MEMBERS")) {
+            return message.channel.send(`You do not have the correct permissions to do this command, ${message.author}`)
+        }
+        if(!message.guild.me.hasPermission("KICK_MEMBERS")) {
+            return message.channel.send(`You do not have the correct permissions to do this command, ${message.author}`)
+        }
+
+        let target = message.mentions.members.first();
+
+        if(!target) {
+            return message.channel.send(`Please specify who you want to kick, ${message.author}`)
+        }
+        if(!target.kickable) {
+            return message.channel.send(`I can not kick this user because they have a role higher than me.`)
+        }
+        if(target.id === message.author.id) {
+            return message.channel.send(`You can not kick yourself, dummy.`)
+        }
+        if(!args[1]) {
+            return message.channel.send(`Please specify a reason to kick someone. Don't be stupid.`)
+        }
+
+        const Embed = new Discord.MessageEmbed()
+        .setColor(`BLUE`)
+        .setTitle(`**__${message.author.username}__ has kick someone :wave:**`)
+        .setDescription(`${message.author} has kicked ${target} (${target.id})`)
+        .setFooter(`Well, goodbye for them.`)
+        message.channel.send(Embed)
+
+        target.kick(args[1])
+    }
+}
