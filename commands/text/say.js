dc92b0a569d18139821155308e49ca73b27b8624
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-06-22 17:33:07 -0500

    Update say.js

--- commands/text/say.js
+++ commands/text/say.js
@@ -3,6 +3,8 @@
     description: "Bot says stuff",
     usage: "<msg>",
     run: async (bot, message, args) => {
+      const user = message.author
+      if (user.id === "514530713312428054") return message.channel.send('ur not allowed to use this specter');
       const sayMessage = args.join(" ");
       message.delete().catch(O_o=>{}); 
       message.channel.send(sayMessage);
