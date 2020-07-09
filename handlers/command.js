936e69194e4c1f06eefb39c3b4f2d07cec67851c
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-07-08 23:13:01 -0500

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,15 +1,20 @@
-const { readdirSync } = require("fs");
-module.exports = (bot) => {
-    readdirSync("./commands/").map(dir => {
-       const commands = readdirSync(`./commands/${dir}/`).map(cmd=>{
-           let pull = require(`../commands/${dir}/${cmd}`)
-           console.log(`Loaded command ${pull.name}! ✅`)
-           bot.commands.set(pull.name,pull)
-           if(cmd.aliases){
-               cmd.aliases.forEach(p=>{
-                   bot.aliases.set(p,pull)
-               })
-           }
-       })
+const {readdirSync} = require('fs')
+const ascii = require('ascii-table')
+let table = new ascii("Commands");
+table.setHeading('Command', "Load Status");
+module.exports= (bot)=>{
+    readdirSync('./commands/').forEach(dir=>{
+        const commands = readdirSync(`./commands/${dir}/`).filter(file=>file.endsWith('.js'));
+        for(let file of commands){
+            let pull = require(`../commands/${dir}/${file}`);
+            if(pull.name){
+                bot.commands.set(pull.name, pull)
+                table.addRow(file, "✅")
+            } else {
+                table.addRow(file, `❌ -> Missing a help.name, or help.name in not a string`)
+                continue;
+            } if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name))
+        }
     })
+    console.log(table.toString())
 }
