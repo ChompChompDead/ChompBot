71ee21afea47357a967a3d75f3eb2dc13898fadd
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-07-11 15:43:41 -0500

    Add files via upload

---
+++ commands/fun/rps.js
@@ -0,0 +1,40 @@
+const { MessageEmbed } = require('discord.js')
+const { promptMessage } = require('../../function.js')
+
+const chooseArr = ['⛰️','📰','✂️'];
+
+module.exports={
+  name:"rps",
+  description: "Play rock, paper, and scissors with a bot",
+  run: async(client,message,args)=>{
+    const embed = new MessageEmbed()
+    .setColor(1752220)
+    .setFooter(client.user.username, client.user.displayAvatarURL)
+    .setDescription(`**Add a reaction to one of these emojis to play the game!**\n(Please wait for all emojis to load first.)`)
+    .setTimestamp()
+    const m = await message.channel.send(embed)
+    const reacted = await promptMessage(m, message.author, 30, chooseArr);
+
+    const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
+
+    const result = await getResult(reacted, botChoice)
+    await m.reactions.removeAll();
+
+    embed
+    .setDescription("")
+    .addField(result, `${reacted} vs ${botChoice}`)
+    m.edit(embed)
+
+    function getResult(me, clientChosen) {
+      if((me === "⛰️" && clientChosen === "✂️") ||
+         (me === "📰" && clientChosen === "⛰️") ||
+         (me === "✂️" && clientChosen === "📰")) {
+            return "You won! :D";
+         } else if (me === clientChosen) {
+            return "Its a tie!"
+         } else {
+            return "You lost :("
+         }
+    }
+  }
+}
