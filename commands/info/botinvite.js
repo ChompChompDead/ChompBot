f91bd95ed59ef33fa2b2d3cfc41de0947eabb711
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-07-16 21:57:14 -0500

    Update botinvite.js

--- commands/info/botinvite.js
+++ commands/info/botinvite.js
@@ -6,7 +6,7 @@ module.exports={
     run: async(bot,message,args)=>{
         const Embed = new MessageEmbed()
         .setTitle('ChompBot Invite Link')
-        .setDescription(`To invite ChompBot, click on this link below.\nhttp://sites.google.com/view/chompbot`)
+        .setDescription(`To invite ChompBot, click on this link below.\nhttp://chompbot.ml`)
         .setFooter('Have fun with ChompBot! | A ChompBot Message.')
         .setColor(1752220)
         message.channel.send(Embed)
