dadbcc3de86a74c80af6e36ec254d1724f66f252
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2020-07-23 19:17:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,23 @@ const activities_list = [
     "Version 0.7 | c!help",
     `stop looking at this status NOW`, 
     "use c!help already"
-    ];
+];
+
+bot.on('guildMemberAdd', (member) => {
+    let chx = db.get(`wChannel_${member.guild.id}`)
+
+    if(!chx === null) {
+        return;
+    }
+
+    let embed = new Discord.MessageEmbed()
+    .setAuthor(member.user.username + "has joined the server!")
+    .setColor(1752220)
+    .setThumbnail(member.user.displayAvatarURL())
+    .setDescription(`Welcome to the server, **${member.user.username}**! Be sure to check the rules and have fun!`)
+
+    bot.channels.cache.get(chx).send(embed)
+})
 
 bot.on('ready', () => {
     setInterval(() => {
